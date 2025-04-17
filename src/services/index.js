a5865275454f2e57e1f1d2c54826c46822153c1b
luansilva25 <luansilva250807@gmail.com>
2025-04-17 11:33:38 -0300

    FEAT: Create Batch Stores and Services [refs: #8]

--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,11 @@
 import serviceExample from "./serviceExample";
 import ColorService from './color'
 import AddressService from './address'
+import BatchService from './address'
 
 export {
     AddressService,
     ColorService,
+    BatchService,
     serviceExample,
-
 }
