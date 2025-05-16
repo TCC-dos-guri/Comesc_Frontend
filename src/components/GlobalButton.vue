4b3bd1f5606a7b69e82fb6928906d9bb5cec2600
renascm <renatochagas.m@gmail.com>
2025-05-16 20:57:52 -0300

    STYLE: Refactor GlobalButton.vue for code consistency, formatting and add emit ref[#14]

--- src/components/GlobalButton.vue
+++ src/components/GlobalButton.vue
@@ -1,27 +1,35 @@
 <script setup>
+defineEmits(["click"]);
 defineProps({
   btnStyle: {
     type: [String, Array],
-    default: 'bg-red-500 text-white size-auto rounded-full',
+    default: "bg-red-500 text-white size-auto rounded-full",
   },
   title: {
     type: String,
-    default: 'text-here',
+    default: "text-here",
   },
   extraDiv: {
     type: Boolean,
     default: true,
-  }
-})
+  },
+});
 </script>
 
 <template>
-  <button :class="['flex items-center justify-between h-12 px-4 gap-2 rounded-full bg-[#261D47]', btnStyle]">
+  <button
+    :class="[
+      'flex items-center justify-between h-12 px-4 gap-2 rounded-full bg-[#261D47]',
+      btnStyle,
+    ]"
+    @click="$emit('click')"
+  >
     <span class="flex-1 text-center text-white">{{ title }}</span>
-    <i v-if="extraDiv" class="mdi mdi-chevron-right flex items-center justify-center w-9 h-9 text-[#261D47] text-3xl bg-white rounded-full "></i>
+    <i
+      v-if="extraDiv"
+      class="mdi mdi-chevron-right flex items-center justify-center w-9 h-9 text-[#261D47] text-3xl bg-white rounded-full"
+    ></i>
   </button>
-
-
 
   <!--
   <GlobalButton :btnStyle="['']" title="Proximo passo" :extraDiv="true" /
