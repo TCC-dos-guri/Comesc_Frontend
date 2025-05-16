40ee54bfbde47d38c8ddf035330d7dbd2e1b2915
renascm_ <renatochagas.m@gmail.com_>
2025-05-16 09:14:55 -0300

    CHORE: global button

---
+++ src/components/GlobalButton.vue
@@ -0,0 +1,28 @@
+<script setup>
+  const props = defineProps({
+    btnStyle: {
+      type: String,
+      default: '[bg-red-500 text-white w-32 h-12 rounded-lg]',
+      required: true,
+    },
+    tittle: {
+      type: String,
+      default: 'texto',
+      required: true,
+    },
+    extraDiv:{
+      type: Boolean,
+      default: false,
+    }
+  })
+</script>
+
+<template>
+  <button :class="btnStyle">
+    {{ tittle }}
+    <div v-if="$props.extraDiv">
+
+    </div>
+  </button>
+
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,15 @@
-
+<script setup>
+import GlobalButton from '@/components/GlobalButton.vue'
+</script>
 
 <template>
-  <main>
-    <h1>Hello World</h1>
-  </main>
+  <GlobalButton class="teste" :btnStyle="['bg-red-500', 'text-white', 'w-32', 'h-12', 'rounded-lg']" tittle="texto">
+    <slot>
+      
+    </slot>
+  </GlobalButton>
 </template>
+
+<style scoped>
+
+</style>
