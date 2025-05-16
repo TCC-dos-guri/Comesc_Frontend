3bc76ea5b8cef1ca35f77811b92e603f7e9d9f60
renascm <renatochagas.m@gmail.com>
2025-05-16 20:42:25 -0300

    FEAT: Created global button component

--- src/components/GlobalButton.vue
+++ src/components/GlobalButton.vue
@@ -1,28 +1,29 @@
 <script setup>
-  const props = defineProps({
-    btnStyle: {
-      type: String,
-      default: '[bg-red-500 text-white w-32 h-12 rounded-lg]',
-      required: true,
-    },
-    tittle: {
-      type: String,
-      default: 'texto',
-      required: true,
-    },
-    extraDiv:{
-      type: Boolean,
-      default: false,
-    }
-  })
+defineProps({
+  btnStyle: {
+    type: [String, Array],
+    default: 'bg-red-500 text-white size-auto rounded-full',
+  },
+  title: {
+    type: String,
+    default: 'texto',
+  },
+  extraDiv: {
+    type: Boolean,
+    default: true,
+  }
+})
 </script>
 
 <template>
-  <button :class="btnStyle">
-    {{ tittle }}
-    <div v-if="$props.extraDiv">
-
-    </div>
+  <button :class="['flex items-center justify-between h-12 px-4 gap-2 rounded-full bg-[#261D47]', btnStyle]">
+    <span class="flex-1 text-center text-white">{{ title }}</span>
+    <i v-if="extraDiv" class="mdi mdi-chevron-right flex items-center justify-center w-9 h-9 text-[#261D47] text-3xl bg-white rounded-full "></i>
   </button>
 
+
+
+  <!--
+  <GlobalButton :btnStyle="['']" title="Proximo passo" :extraDiv="true" / 
+  -->
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,13 +3,5 @@ import GlobalButton from '@/components/GlobalButton.vue'
 </script>
 
 <template>
-  <GlobalButton class="teste" :btnStyle="['bg-red-500', 'text-white', 'w-32', 'h-12', 'rounded-lg']" tittle="texto">
-    <slot>
-      
-    </slot>
-  </GlobalButton>
+  
 </template>
-
-<style scoped>
-
-</style>
