bd95fa118fe5d024c0fbe7e1c4d4b4aa2947c048
luansilva25 <luansilva250807@gmail.com>
2025-05-30 17:48:42 -0300

    FEAT: create input global component #15

---
+++ src/components/global/GlobalInput.vue
@@ -0,0 +1,24 @@
+<script setup>
+    defineProps({
+        placeholder: {
+            type: String,
+            default: ''
+        }, 
+        label: {
+            type: String,
+            default: ''
+        },
+        class_input: {
+            type: String,
+            default: ''
+        }
+    })
+
+    const model = defineModel('value')
+</script>
+<template>
+    <div class="flex flex-col ga-2">
+        <label for="input" v-if="label">{{ label }}</label>
+        <input type="text" :placeholder="placeholder" :class="class_input" v-model="model">
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <script setup>
-import GlobalButton from '@/components/GlobalButton.vue'
+import {GlobalButton, GlobalInput} from '@/components/index'
 </script>
 
 <template>
-  
+    <GlobalButton/>
+    <GlobalInput class_input="border ps-2 border-neutral-400 rounded-xl w-96 outline-0 p-2" placeholder="teste" label="teste"/>
 </template>
