04b661767af66bbc7e9daeb635b04e1e076ebca6
Gabz047 <gabe1612mail@gmail.com>
2025-06-13 08:25:19 -0300

    FEAT: Added vite api base url

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+const apiURL = import.meta.env.VITE_API_URL
 //Allows the application to make requisition with the backend
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api/',
+    baseURL: apiURL,
     headers: {
         "Content-Type": "application/json"
     }
