da4e84ca033994b6822d2709b4f0b0167a48ad9b
Gabz047 <gabe1612mail@gmail.com>
2025-10-27 20:13:21 -0300

    FEAT: Adding global button

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
+<script setup>
+import GlobalButton from './components/global/buttons/GlobalButton.vue';
+</script>
+
 <template>
+  <GlobalButton extra-div="false" />
   <RouterView />
 </template>
 
